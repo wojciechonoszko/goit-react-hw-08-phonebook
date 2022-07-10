7e2da2e6be0266321be2a90110f02800e6a697a6
Wojciech <wojciechonoszko@gmail.com>
2022-07-10 23:16:40 +0200

    refactor contactForm

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+//import PropTypes from 'prop-types';
 import{
     ButtonContainer,
     ContactFormContainer,
@@ -7,80 +8,144 @@ import{
     LabelContainer
 } from './ContactFormStyles';
 
-import { connect } from 'react-redux';
-import { addNewContact } from '../../redux/contacts/contacts-actions';
+//import { connect } from 'react-redux';
+import { addNewContact, getContacts } from '../../redux/contacts/contacts-actions';
 
-
-class ContactForm extends Component {
-
-    state = {
-        name: '',
-        number: ''
+export default function ContactForm() {
+    const [name, setName] = useState('');
+    const handleNameChange = event => {
+      setName(event.target.value);
+    };
+  
+    const [number, setNumber] = useState('');
+    const handleNumberChange = event => {
+      setNumber(event.target.value);
+    };
+  
+    const contacts = useSelector(getContacts);
+  
+    const dispatch = useDispatch();
+  
+    const addNew = contact => dispatch(addNewContact(contact));
+  
+    const handleSubmit = event => {
+      event.preventDefault();
+  
+    //   if (contacts.some(contact => contact.name === name)) {
+    //     alert(`${name} is already in contacts.`);
+    //     return;
+    //   }
+  
+      addNew({
+        name,
+        number
+      });
+  
+      setName('');
+      setNumber('');
     };
 
-    handleChange = event => {
-        this.setState({[event.target.name]: event.target.value});
+    return (
+        <ContactFormContainer onSubmit={handleSubmit}>
+            <LabelContainer htmlFor="name">Name</LabelContainer>
+            <InputContainer
+            type='text'
+            id='name'
+            name='name'
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name like Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan etc."
+            required
+            onChange={handleNameChange}
+            value={name}
+        />
+            <LabelContainer htmlFor="number">Number</LabelContainer>
+            <InputContainer
+            type='tel'
+            id='number'
+            name='number'
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={handleNumberChange}
+            value={number}
+        />
+        <ButtonContainer type="submit">Add contact</ButtonContainer>
 
-    };
+        </ContactFormContainer>
+    );
 
-    handleSubmit = event => {
-        event.preventDefault();
-
-        this.props.addNewContact({
-            name: this.state.name,
-            number: this.state.number
-        });
-    };
-
-    render() {
-        return (
-            <ContactFormContainer onSubmit={this.handleSubmit}>
-                <LabelContainer htmlFor="name">Name</LabelContainer>
-                <InputContainer
-                type='text'
-                id='name'
-                name='name'
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name like Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan etc."
-                required
-                onChange={this.handleChange}
-                value={this.state.name}
-            />
-                <LabelContainer htmlFor="number">Number</LabelContainer>
-                <InputContainer
-                type='tel'
-                id='number'
-                name='number'
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-                onChange={this.handleChange}
-                value={this.state.value}
-            />
-            <ButtonContainer type="submit">Add contact</ButtonContainer>
-
-            </ContactFormContainer>
-        );
-    }
 }
 
-ContactForm.propTypes = {
-    addNewContact: PropTypes.func.isRequired
-};
+// class ContactForm extends Component {
 
-const mapStateToProps = state => {
-    return {
-        contacts: state.contacts
-    };
-};
+//     state = {
+//         name: '',
+//         number: ''
+//     };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        addNewContact: contact => dispatch(addNewContact(contact))
-    }
-}
+//     handleChange = event => {
+//         this.setState({[event.target.name]: event.target.value});
 
-export default connect(mapStateToProps, mapDispatchToProps)(ContactForm);
+//     };
+
+//     handleSubmit = event => {
+//         event.preventDefault();
+
+//         this.props.addNewContact({
+//             name: this.state.name,
+//             number: this.state.number
+//         });
+//     };
+
+//     render() {
+//         return (
+//             <ContactFormContainer onSubmit={this.handleSubmit}>
+//                 <LabelContainer htmlFor="name">Name</LabelContainer>
+//                 <InputContainer
+//                 type='text'
+//                 id='name'
+//                 name='name'
+//                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//                 title="Name like Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan etc."
+//                 required
+//                 onChange={this.handleChange}
+//                 value={this.state.name}
+//             />
+//                 <LabelContainer htmlFor="number">Number</LabelContainer>
+//                 <InputContainer
+//                 type='tel'
+//                 id='number'
+//                 name='number'
+//                 pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//                 required
+//                 onChange={this.handleChange}
+//                 value={this.state.value}
+//             />
+//             <ButtonContainer type="submit">Add contact</ButtonContainer>
+
+//             </ContactFormContainer>
+//         );
+//     }
+// }
+
+// ContactForm.propTypes = {
+//     addNewContact: PropTypes.func.isRequired
+// };
+
+// const mapStateToProps = state => {
+//     return {
+//         contacts: state.contacts
+//     };
+// };
+
+// const mapDispatchToProps = dispatch => {
+//     return {
+//         addNewContact: contact => dispatch(addNewContact(contact))
+//     }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(ContactForm);
 
 
 
--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -3,3 +3,5 @@ import { createAction } from '@reduxjs/toolkit';
 export const addNewContact = createAction('contacts/Add');
 
 export const deleteContact = createAction('contacts/Delete');
+
+export const getContacts = state => state.contacts.contacts;
