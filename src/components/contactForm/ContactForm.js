48c018f631146fb5826457b34aa7e6197d4dcc37
Wojciech <wojciechonoszko@gmail.com>
2022-07-22 20:44:14 +0200

    Update ContactForm.js

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-//import { useState } from "react";
-
 import { useDispatch, useSelector } from 'react-redux';
 
 import { addNewContact } from '../../redux/contacts/contacts-actions'
-//import PropTypes from 'prop-types';
 import{
     ButtonContainer,
     ContactFormContainer,
@@ -68,72 +65,3 @@ export default function ContactForm({ onSubmit }) {
         </ContactFormContainer>
     );
 }
-// export default function ContactForm({ onSubmit }) {
-//     const [name, setName ] = useState("");
-//     const [number, setNumber] = useState("");
-
-//     const handleInput = (e) => {
-//         const { name, value } = e.target;
-//         switch (name) {
-//             case "name":
-//                 setName(value);
-//                 break;
-//             case "number":
-//                 setNumber(value);
-//                 break;
-
-//             default:
-//                 break
-//         }
-//     };
-
-//     const dispatch = useDispatch();
-
-//     // const handleSubmit = (e) => {
-//     //     e.preventDefault();
-//     //     onSubmit({ name, number });
-//     //     reset();
-//     // };
-
-//     const handleAddContact = (e) => {
-//         e.preventDefault();
-//         dispatch(addContact({ setName, setNumber }));
-//         reset();
-//     }
-
-//     const reset = () => {
-//         setName("");
-//         setNumber("");
-//     };
-
-    
-
-//     return (
-//         <ContactFormContainer onSubmit={handleAddContact}>
-//             <LabelContainer htmlFor="name">Name</LabelContainer>
-//             <InputContainer
-//             type='text'
-//             id='name'
-//             name='name'
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name like Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan etc."
-//             required
-//             onChange={handleInput}
-//             value={name}
-//         />
-//             <LabelContainer htmlFor="number">Number</LabelContainer>
-//             <InputContainer
-//             type='tel'
-//             id='number'
-//             name='number'
-//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//             required
-//             onChange={handleInput}
-//             value={number}
-//         />
-//         <ButtonContainer type="submit">Add contact</ButtonContainer>
-
-//         </ContactFormContainer>
-//     );
-// }
