51e80a055ee0236186ef868c34b5f6d3ba61ed25
Wojciech <wojciechonoszko@gmail.com>
2022-07-25 19:44:53 +0200

    updating

---
+++ src/components/userMenu/UserMenu.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { useDispatch, useSelector } from "react-redux";
+import { authOperations, authSelectors } from "../../redux/auth";
+import defaultAvatar from './defaultAvatar.png';
+import styles from './UserMenu.module.css';
+
+export default function UserMenu() {
+    const dispatch = useDispatch();
+    const name = useSelector(authSelectors.getUserName);
+    const avatar = defaultAvatar;
+
+
+return (
+    <div className={ styles.userMenu }>
+    <img src={avatar} alt="avatar" width="32"  />
+    <span className={styles.userName}>Helo, my friend {name}</span>
+    <Stack spacing={2} direction="row">
+      <Button type="button"
+        variant="contained"
+        className={styles.button}
+        onClick={() => dispatch(authOperations.logOut())}>
+      Logout
+      </Button>
+    </Stack>
+  </div>
+);
+}
